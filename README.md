97d814b0ec7b2a2d6d23fa693302750e1f53874c
chukwudinweze <chukwudinwez@gmail.com>
2021-01-19 21:11:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-router-project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-router-project)
---
+++ src/components/ExpenseDashBoard.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+const ExpenseDashBoard = () => <div>This is from my dashboard</div>;
+
+export default ExpenseDashBoard;
